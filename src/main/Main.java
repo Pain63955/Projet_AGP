ace7550defffd14bd9eca6a7c65591a03410caf8
Pain63955 <arnau@MSI.lan>
2026-01-16 08:37:16 +0100

    Maj name package

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,8 @@
-package test;
+package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello world !!!");
 
 	}
