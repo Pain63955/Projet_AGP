38f94802637ec5c5a3e5e215245a69d524b57094
Adesiles <adam.desiles@gmail.com>
2026-01-16 13:38:06 +0100

    Push clean

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
 package main;
-
-/**
- * La classe Main est la classe principale de l'application.
- * Elle contient la méthode main qui est le point d'entrée du programme.
- * 
- * @author Pain63955
- * @version 1.0
- */
-public class Main {
-
-    /**
-     * Point d'entrée de l'application.
-     * Cette méthode est exécutée lorsque le programme est lancé.
-     *
-     * @param args Tableau de chaînes de caractères représentant les arguments passés en ligne de commande.
-     */
-    public static void main(String[] args) {
-        // Affiche "hello world !!!" dans la console
-        System.out.println("hello world !!!");   
-      //G commit bonjour (devinez qui c'est ?)
-    }
-<<<<<<< HEAD
-//treatjkdfhgkjdfgkjdfg
-=======
->>>>>>> branch 'master' of https://github.com/Pain63955/Projet_AGP.git
-}
-    
-    
-    
-=======
-<<<<<<< HEAD
-package main;
-
-/**
- * La classe Main est la classe principale de l'application.
- * Elle contient la méthode main qui est le point d'entrée du programme.
- * 
- * @author Pain63955
- * @version 1.0
- */
-public class Main {
-
-    /**
-     * Point d'entrée de l'application.
-     * Cette méthode est exécutée lorsque le programme est lancé.
-     *
-     * @param args Tableau de chaînes de caractères représentant les arguments passés en ligne de commande.
-     */
-    public static void main(String[] args) {
-        // Affiche "hello world !!!" dans la console
-        System.out.println("hello world !!!");       
-    }
-//test
-}
-=======
-package main;
-
 /**
  * La classe Main est la classe principale de l'application.
  * Elle contient la méthode main qui est le point d'entrée du programme.
@@ -77,6 +19,3 @@ public class Main {
         System.out.println("hello world !!!");       
     }
 }
->>>>>>> branch 'master' of https://github.com/Pain63955/Projet_AGP.git
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>la
->>>>>>> branch 'master' of https://github.com/Pain63955/Projet_AGP.git
