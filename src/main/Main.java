8b8f96be98c82769e7ec98dec17e38439a134823
Pain63955 <arnau@10.77.80.93>
2026-01-16 13:17:24 +0100

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // Affiche "hello world !!!" dans la console
-        System.out.println("hello world !!!");      
+        System.out.println("hello world !!!");       
     }
 
 }
