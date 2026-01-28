f7389c99eee6d75c79968970fd47fa3f02ff2480
Adesiles <adam.desiles@gmail.com>
2026-01-28 14:43:34 +0100

    parser

--- src/main/java/api/core/BDeQueryParser.java
+++ src/main/java/api/core/BDeQueryParser.java
@@ -1,5 +1,18 @@
 package api.core;
 
 public class BDeQueryParser {
-
+	
+	BDeQueryParser(){
+		
+	}
+	
+	public String sqlPart(String input) {
+		String parts[] = input.split("WITH");
+		return parts[0];
+	}
+	
+	public String textPart(String input) {
+		String parts[] = input.split("WITH");
+		return parts[1];
+	}
 }
