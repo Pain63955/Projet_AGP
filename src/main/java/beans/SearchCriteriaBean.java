d47d465ba90ddae2c63435287f986e52badc7e16
Adesiles <adam.desiles@gmail.com>
2026-01-28 10:28:58 +0100

    search bean criteria

--- src/main/java/beans/SearchCriteriaBean.java
+++ src/main/java/beans/SearchCriteriaBean.java
@@ -1,16 +1,50 @@
 package beans;
 
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import business.offer.SearchCriteria;
 
 @ManagedBean
 @SessionScoped
-public class SearchCriteriaBean {
+public class SearchCriteriaBean implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2204740088413345478L;
 	private int nbJours;
 	private double budgetMax;
+	
+	private SearchCriteria srct;
+
+	public String search() {
+		return null;
+	}
+	
+	public void validatePrice(FacesContext context, UIComponent componentToValidate, Object value) throws ValidatorException {
+		int prix = (int)value;
+		
+		if (prix < 0 || prix > 50000) {
+			FacesMessage message = new FacesMessage("The prix cannot be negative nor over 50000€");
+			throw new ValidatorException(message);
+		}
+	}
+	
+	public void validateJours(FacesContext context, UIComponent componentToValidate, Object value) throws ValidatorException {
+		int days = (int)value;
+		
+		if (days < 0 || days > 100) {
+			FacesMessage message = new FacesMessage("The amount of days cannot be less than 1 and above 100");
+			throw new ValidatorException(message);
+		}
+	}
 	
 	public SearchCriteriaBean() {
 		
@@ -24,6 +58,15 @@ public class SearchCriteriaBean {
 		this.nbJours = nbJours;
 	}
 
+	public double getBudgetMin() {
+		return this.srct.getBudgetMax();
+		
+	}
+	
+	public void setBudgetMin() {
+		
+	}
+	
 	public double getBudgetMax() {
 		return budgetMax;
 	}
--- src/main/java/business/offer/SearchCriteria.java
+++ src/main/java/business/offer/SearchCriteria.java
@@ -3,33 +3,68 @@ package business.offer;
 public class SearchCriteria {
     private String destination;     // Pour filtrer la zone géographique
     private int nbDays;           // Utilise la boucle i < nbJours dans le Builder
+    private double budgetMin;
     private double budgetMax;      // La limite pour la méthode estDansLeBudget() de OffreSejour
     private String keywords;       // La chaîne de texte analysée par l'IA (ex: "zen temple surf")
     private String askedTransport; // Le mode envoyé à la factory (BATEAU, BUS, etc.)
 
     public SearchCriteria() {}
 
-    public SearchCriteria(String destination, int nbJours, double budgetMax, String motsCles, String transportSouhaite) {
+    public SearchCriteria(String destination, int nbJours,double budgetMin, double budgetMax, String motsCles, String transportSouhaite) {
         this.destination = destination;
         this.nbDays = nbJours;
+        this.budgetMin = budgetMin;
         this.budgetMax = budgetMax;
         this.keywords = motsCles;
         this.askedTransport = transportSouhaite;
     }
 
-    // --- GETTERS ET SETTERS ---
-    public String getDestination() { return destination; }
-    public void setDestination(String destination) { this.destination = destination; }
+	public String getDestination() {
+		return destination;
+	}
 
-    public int getNbDays() { return nbDays; }
-    public void setNbDays(int nbJours) { this.nbDays = nbJours; }
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
 
-    public double getBudgetMax() { return budgetMax; }
-    public void setBudgetMax(double budgetMax) { this.budgetMax = budgetMax; }
+	public int getNbDays() {
+		return nbDays;
+	}
 
-    public String getKeywords() { return keywords; }
-    public void setKeywords(String keywords) { this.keywords = keywords; }
+	public void setNbDays(int nbDays) {
+		this.nbDays = nbDays;
+	}
 
-    public String getAskedTransport() { return askedTransport; }
-    public void setAskedTransport(String askedTransport) { this.askedTransport = askedTransport; }
+	public double getBudgetMin() {
+		return budgetMin;
+	}
+
+	public void setBudgetMin(double budgetMin) {
+		this.budgetMin = budgetMin;
+	}
+
+	public double getBudgetMax() {
+		return budgetMax;
+	}
+
+	public void setBudgetMax(double budgetMax) {
+		this.budgetMax = budgetMax;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public String getAskedTransport() {
+		return askedTransport;
+	}
+
+	public void setAskedTransport(String askedTransport) {
+		this.askedTransport = askedTransport;
+	}
+
 }
