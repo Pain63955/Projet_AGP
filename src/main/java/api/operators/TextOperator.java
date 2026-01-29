bd1e80b307cae9210a8542e3aaa507e53162e6a3
chloe_ferron <chloe.ferron@etu.u-cergy.fr>
2026-01-29 10:31:34 +0100

    Merge branch 'master' of https://github.com/Pain63955/Projet_AGP.git

--- src/main/java/api/operators/TextOperator.java
+++ src/main/java/api/operators/TextOperator.java
@@ -1,8 +1,41 @@
 package api.operators;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import api.core.BDeConfig;
+import api.text.LuceneIndexService;
 import api.visitor.OperatorVisitor;
 
 public class TextOperator extends Operator {
+	
+	private BDeConfig bdeConfig;
+	private String textPart;
+	
+	private HashMap<Integer, Double> scoreByKey;
+	private ArrayList<Integer> keysOrdered;
+	
+	public LuceneIndexService lucenneIndex;
+	
+	public TextOperator(String textPart, BDeConfig bdeConfig) {
+		super();
+		this.textPart = textPart;
+		this.bdeConfig = bdeConfig;
+	} 
+	
+	public void open() throws Exception {
+		lucenneIndex=new LuceneIndexService(bdeConfig);
+		this.scoreByKey = lucenneIndex.search(textPart);
+		this.keysOrdered = lucenneIndex.sortScores(scoreByKey);
+	}
+	
+	public HashMap<Integer, Double> getScoreByKey(){
+		return scoreByKey;
+	}
+	
+	public ArrayList<Integer> getKeysOrdered(){
+		return keysOrdered;
+	}
 	
 	@Override
 	public void accept(OperatorVisitor visitor) {
